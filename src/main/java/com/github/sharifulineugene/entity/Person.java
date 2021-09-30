df4becd7820bb26c22c8149d3641a9090f06bd85
Шарифулин Евгений Юрьевич <EYSharifulin@sberbank.ru>
2021-09-30 16:12:30 +0700

    Создан класс Person

---
+++ src/main/java/com/github/sharifulineugene/entity/Person.java
@@ -0,0 +1,44 @@
+package com.github.sharifulineugene.entity;
+
+public class Person {
+    private final int id;
+    private String name, surname;
+    private String dateOfBirth;
+
+    public Person(int id, String name, String surname, String dateOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+
+}
